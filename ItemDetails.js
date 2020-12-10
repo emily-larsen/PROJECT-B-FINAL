7f26076197e77ec54e49996491d6691810a1bc30
EmilyLarsen <73974031+emily-larsen@users.noreply.github.com>
2020-12-10 03:32:15 -0500

    Update ItemDetails.js

--- ItemDetails.js
+++ ItemDetails.js
@@ -1,30 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Image, Button, StyleSheet, Text, View } from 'react-native';
+iimport { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, Image, Button, Text, View } from 'react-native';
 
-const ItemDetails= ( {navigation, route} ) => {
+  
 
-  const { name, image } = route.params
+const ItemDetails = ({ navigation, route }) => {
+
+  const [value, onChangeText] = React.useState('Enter Text');
+  const { image } = route.params;
 
   const goBackToList = () => {
-    navigation.navigate('Item List');
+    navigation.navigate('Ems Photos');
   }
+
+  
 
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={image} />
+      
+      <Image style={styles.image}
+             source={image} />
       <Text style={styles.title}>
-        Item Details Screen
+		  {}
       </Text>
 
-      <Button title="Go to back to list"
-              onPress={goBackToList}/>
+      <Text>
+		    Photo Name:
+      </Text>
+    <TextInput 
+    style={styles.input} 
+    placeholder='e.g The Woods'
+    />
+
+  <Text>
+		    Photo Date:
+      </Text>
+    <TextInput 
+    style={styles.input} 
+    placeholder='e.g 11/11/2011'/>
+
+    <Text>
+		    Photo Location:
+      </Text>
+    <TextInput 
+    style={styles.input} 
+    placeholder='e.g  41°2412.2"N 2°E'/>
+
+  <Text>
+		    Notes/Comments:
+      </Text>
+    <TextInput 
+    multiline
+    style={styles.input} 
+    placeholder='e.g Nice Photo'/>
+
+    
+  <Button title="Save"
+              onPress={goBackToList} />
+
+    <Button title="Go Back"
+              onPress={goBackToList} />
+
       <StatusBar style="auto" />
 
     </View>
   );
 }
+
 export default ItemDetails;
+
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -32,17 +80,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'flex-start',
-
   },
   title: {
     fontSize: 36,
-    margin: 20
+    marginBottom: 20,
   },
   image: {
-    width: '100%',
-    height: 200,
+    width: '90%',
+    height: 350,
     resizeMode: 'cover',
-    marginBottom: 10
-}
+    marginBottom: 0,
+    marginTop: 18,
+  },
+  input: {
+    borderWidth: 3,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 300,
 
+  }
 });
