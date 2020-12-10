5f0f4d4a89ceb7d08c5c17c9fbcdd407a1aabe7e
EmilyLarsen <73974031+emily-larsen@users.noreply.github.com>
2020-12-10 03:32:56 -0500

    Update ItemList.js

--- ItemList.js
+++ ItemList.js
@@ -1,32 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useEffect} from 'react';
-import { TouchableOpacity, Image, SafeAreaView, FlatList, Button, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { Dimensions, TouchableOpacity, Image, SafeAreaView, FlatList, Button, StyleSheet, Text, View } from 'react-native';
 
-const ItemList = ({ route, navigation }) => {
-
-  const { items } = route.params
-
-    useEffect( () => {
-      console.log(items)
-    })
+const ItemList = ({ route, navigation}) => {
   
-const goToDetails = (item) => {
-  navigation.navigate('Item Details', {name: item.name, image: item.image});
-}
+  const { items } = route.params
+  
+  useEffect( () => {
+    console.log(items)
+  })
 
-const itemRow = ({ item }) => (
-  <TouchableOpacity 
-    style={styles.cell}
-    onPress={() => goToDetails(item)}
-    >
+  const goToDetails = (item) => {
+    navigation.navigate('Edit Photo', {name: item.name, image: item.image});
+  }
 
-    <Image style={styles.image} source={item.image} />
-   
-
-  </TouchableOpacity>
-);
-
+  const itemRow = ({ item }) => (
+    <TouchableOpacity
+      style={styles.cell}
+      onPress={() => goToDetails(item)} >
+      <Image 
+        style={styles.image}
+        source={item.image}/>
+    </TouchableOpacity>
+  );
+  
+  const columnWidth=Math.floor(Dimensions.get('window').width/styles.image.width)
+  
+  console.log("columnWidth:"+columnWidth);
+  
+  
   return (
+    
     <SafeAreaView style={styles.grid}>
       <FlatList
         contentContainerStyle={styles.grid}
@@ -34,12 +38,16 @@ const itemRow = ({ item }) => (
         renderItem={itemRow}
         keyExtractor={item => item.id}
       />
-              
       <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
+
 export default ItemList;
+
+
+
+
 
 
 
@@ -48,34 +56,29 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#FFF',
   },
-
   grid: {
     flex: 1,
     backgroundColor: '#FFF',
     flexDirection: 'row',
     justifyContent: 'space-evenly',
-    flexWrap: 'wrap'
+    flexWrap: 'wrap',
   },
-
   title: {
     fontSize: 24,
-    marginLeft: 20
+    marginLeft: 20,
   },
-
   cell: {
-    width:130,
+    width: 130,
     height: 130,
   },
-
   row: {
     flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
     padding: 20,
     borderBottomColor: '#ccc',
-    borderBottomWidth: 1
+    borderBottomWidth: 1,
   },
-
   image: {
     width: 125,
     height: 125,
